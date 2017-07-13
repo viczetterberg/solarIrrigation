3aca83112a8094d7ddf859ab7eb71551f7730f99
viczetterberg <victor.e.zetterberg@gmail.com>
2017-07-12 20:26:50 -0400

    Add files via upload

---
+++ pump_automation.py
@@ -0,0 +1,38 @@
+import time
+
+from ctrl_sense import Ctrl_sense
+
+# create an isntance of my Ctrl_sense
+ctrl = Ctrl_sense()
+
+i = 0
+pump_threshold = 35  #% moisture content in which pump will be triggered
+irrigation_interval = 40 #seconds pump will be on for
+
+while i < 4:
+    sens0 = ctrl.read_soil()
+    if sens0 == 0:
+        i = 4
+    elif sens0 < 25:
+        i = i + 1
+        print('Test {0}' .format(i))
+        print('Current Soil Moisture %: {0}'.format(sens0))
+        time.sleep(5)
+        sens1 = ctrl.read_soil()
+        i = i + 1
+        print('Test {0}' .format(i))
+        print('Current Soil Moisture %: {0}'.format(sens1))
+        time.sleep(5)
+        sens2 = ctrl.read_soil()
+        i = i + 1
+        print('Test {0}' .format(i))
+        print('Current Soil Moisture %: {0}'.format(sens2))
+        time.sleep(5)
+        sens = (sens0 + sens1 + sens2)/3
+        print('Average Soil Moisture %: {0}'.format(sens))
+
+        if sens < pump_threshold:
+            ctrl.set_pump(True)
+            time.sleep(irrigation_interval)
+            ctrl.set_pump(False)
+            i = i + 1
